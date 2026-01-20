77ed9a9477a3ea30197123562a7b8712b0f050c6
Ermias Alamrew <benim@Ermiass-MacBook-Pro.local>
2026-01-20 10:19:48 +0300

    chore: install react router and tanstack query and setup a sample router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
-import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		children: [
+			{
+				index: true,
+				element: <div>test</div>,
+			},
+		],
+	},
+]);
+
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
+	<QueryClientProvider client={queryClient}>
+		<RouterProvider router={router} />
+	</QueryClientProvider>,
 );
